948a9ef0ad67371ec5545c1a2d27c04076089884
Kemal Utku Lekesiz <kemalutkulekesiz@gmail.com>
2022-07-09 20:47:13 +0300

    Initial Commit

---
+++ gpsStatistics/gpsStatisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpsStatisticsApp.swift
+//  gpsStatistics
+//
+//  Created by Utku Lekesiz on 9.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct gpsStatisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
